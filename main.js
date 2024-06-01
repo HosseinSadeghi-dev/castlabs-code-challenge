3b6732e7660041bb164d94f2601afe45b142f665
Hossein Sadeghi <hskhabbaz@gmail.com>
2024-06-02 00:13:01 +0330

    add find MDAT XML content

--- main.js
+++ main.js
@@ -3,11 +3,36 @@ async function fetchAsArrayBuffer(url) {
     return await response.arrayBuffer();
 }
 
+function parseBoxes(buffer) {
+    const dataView = new DataView(buffer);
+    let offset = 0;
+
+    while (offset < dataView.byteLength) {
+        // first four bytes (bytes 03) specify the size
+        const size = dataView.getUint32(offset);
+        // Bytes 4-7 specify the type of the box
+        const type = String.fromCharCode(
+            dataView.getUint8(offset + 4),
+            dataView.getUint8(offset + 5),
+            dataView.getUint8(offset + 6),
+            dataView.getUint8(offset + 7)
+        );
+
+        if (type === 'mdat') {
+            const content = new TextDecoder("utf-8").decode(new Uint8Array(buffer, offset + 8, size - 8));
+            console.log(`Content of mdat box is: ${content}`);
+        }
+
+        offset += size;
+    }
+}
+
 function main() {
     const url = "http://demo.castlabs.com/tmp/text0.mp4";
     fetchAsArrayBuffer(url)
         .then(arrayBuffer => {
             console.log('Successfully loaded file ', url);
+            parseBoxes(arrayBuffer)
         })
 }
 
